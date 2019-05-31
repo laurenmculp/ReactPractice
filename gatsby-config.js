a14063341b95dd34bd2808b87d5049c9cac531af
Lauren Culp <laurenculp@UX-MacBook-Pro.local>
2019-05-31 10:23:53 -0400

    practice

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Design+Code 3',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,8 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Learn to <br />design and code React apps</h1>
+    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
