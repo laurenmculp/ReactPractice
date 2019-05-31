192755b74de7719a13ea24cd3fbc65623603ecc1
Lauren Culp <laurenculp@UX-MacBook-Pro.local>
2019-05-31 16:27:56 -0400

    homepage: above the fold, fonts, bg image, button, and text styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,17 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Learn to <br />design and code React apps</h1>
-    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to <br />design and code React apps</h1>
+        <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+        <p>Now go build something great.</p>
+        <Link to="/video">Watch the video</Link>
+      </div>
+    </div>
+</div>
+    
   </div>
 )
 
